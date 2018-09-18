ced7d7fcf28744822082cef9d0b4edffae052e35
pauloricardo87 <pauloricardo.sts87@gmail.com>
2018-09-18 11:02:05 -0300

    Fix bug

--- FirmwareIHM_v1_0_Code/mikroC PRO for PIC32/FirmwareIHM_v1_0_events_code.c
+++ FirmwareIHM_v1_0_Code/mikroC PRO for PIC32/FirmwareIHM_v1_0_events_code.c
@@ -130,12 +130,12 @@ void modoPCHost() {
              CheckBox3.Checked = 0;
              CheckBox4.Checked = 0;
              
-             strcpy(Label43.Caption,"PC-Host");
-             strcpy(Label44.Caption,"PC-Host");
-             strcpy(Label48.Caption,"PC-Host");
-             strcpy(Label52.Caption,"PC-Host");
-             strcpy(Label56.Caption,"PC-Host");
-             strcpy(Label60.Caption,"PC-Host");
+             strcpy(Label43.Caption,"PC Host");
+             strcpy(Label44.Caption,"PC Host");
+             strcpy(Label48.Caption,"PC Host");
+             strcpy(Label52.Caption,"PC Host");
+             strcpy(Label56.Caption,"PC Host");
+             strcpy(Label60.Caption,"PC Host");
              
              //ButtonRound8.Transparent = 0;
              DrawScreen(&ScreenOperando);
